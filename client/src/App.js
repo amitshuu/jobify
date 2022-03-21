b6925d42b28aebddd890b82edf2ed8c524253faa
Amit <amitshuu@gmail.com>
2022-03-21 15:54:54 +0200

    React router setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Landing, Error, Register, Dashboard } from "./pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
