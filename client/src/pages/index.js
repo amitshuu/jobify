8b3059597729cb7503b0c03feda3d1fa7678a52a
Amit <amitshuu@gmail.com>
2022-03-21 15:57:55 +0200

    imports

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "../pages/Dashboard";
+import Register from "../pages/Register";
+import Error from "../pages/Error";
+import Landing from "../pages/Landing";
+
+export { Dashboard, Register, Error, Landing };
