73d7c8c94f1930e63fd96a16c69e6182edda07f8
Amit <amitshuu@gmail.com>
2022-03-24 12:56:34 +0200

    User creation with token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,28 @@
-const register = (req, res) => {
-  res.send('Register');
+import User from '../models/User.js';
+import { BadRequestError } from '../errors/index.js';
+
+const register = async (req, res) => {
+  const { name, email, password, lastName } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all values!');
+  }
+  const existUser = await User.findOne({ email });
+  if (existUser) {
+    throw new BadRequestError('User is already exist!');
+  }
+  const user = await User.create({ name, email, password, lastName });
+  const token = user.createJWT();
+  res.status(201).json({
+    user: {
+      name,
+      email,
+      lastName,
+      location: user.location,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = (req, res) => {
