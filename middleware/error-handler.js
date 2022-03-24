c0263ed37a25ca11b3dc7ad8689cec1db81240bd
Amit <amitshuu@gmail.com>
2022-03-24 12:55:29 +0200

    Custom errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,15 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: 'There was an error' });
+  const defaultError = {
+    statusCode: err.statusCode || 500,
+    msg: err.message,
+  };
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = 400;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
