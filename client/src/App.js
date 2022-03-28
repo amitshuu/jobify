e2006f79c1e3be09d3b7fa618fd7607d6bf1f7ed
Amit <amitshuu@gmail.com>
2022-03-28 16:11:02 +0300

    User routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import { Landing, Error, Register, Dashboard } from "./pages";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing, Error, Register, Dashboard } from './pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayouts,
+  Stats,
+} from './pages/dashboard';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayouts />
+            </ProtectedRoute>
+          }
+        >
+          <Route path='/all-jobs' element={<AllJobs />} />
+          <Route path='/add-job' element={<AddJob />} />
+          <Route index element={<Stats />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
