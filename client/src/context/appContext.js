bbe267828970c9b4bd7cdab60c3329c638799e95
Amit <amitshuu@gmail.com>
2022-03-28 16:11:22 +0300

    Refactor and more funcionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -2,12 +2,11 @@ import React, { useContext, useEffect, useReducer, useState } from 'react';
 import {
   CLEAR_ALERT,
   DISPLAY_ALERT,
-  LOGIN_USER_BEGIN,
-  LOGIN_USER_ERROR,
-  LOGIN_USER_SUCCESS,
-  REGISTER_USER_BEGIN,
-  REGISTER_USER_ERROR,
-  REGISTER_USER_SUCCESS,
+  LOGOUT_USER,
+  SETUP_USER_BEGIN,
+  SETUP_USER_ERROR,
+  SETUP_USER_SUCCESS,
+  TOGGLE_SIDEBAR,
 } from './actions';
 import reducer from './reducer';
 import axios from 'axios';
@@ -17,7 +16,7 @@ const user = localStorage.getItem('user');
 const token = localStorage.getItem('token');
 const userLocation = localStorage.getItem('location');
 
-const initialState = {
+export const initialState = {
   isLoading: false,
   showAlert: false,
   alertMsg: '',
@@ -26,6 +25,7 @@ const initialState = {
   token: token,
   userLocation: userLocation || '',
   jobLocation: userLocation || '',
+  showSidebar: false,
 };
 
 export const AppProvider = ({ children }) => {
@@ -35,50 +35,43 @@ export const AppProvider = ({ children }) => {
     dispatch({ type: DISPLAY_ALERT });
   };
 
+  const toggleSidebar = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
+
   const addUserToLocalStorage = ({ user, token, location }) => {
     localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('token', token);
     localStorage.setItem('location', location);
   };
 
-  const removeUserFromLocalStorage = ({ user, token, location }) => {
+  const removeUserFromLocalStorage = () => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
     localStorage.removeItem('location');
   };
 
-  const registerUser = async (currentUser) => {
-    dispatch({ type: REGISTER_USER_BEGIN });
-    try {
-      const response = await axios.post('/api/v1/auth/register', currentUser);
-      const { user, token, location } = response.data;
-      console.log(response);
-      dispatch({
-        type: REGISTER_USER_SUCCESS,
-        payload: { user, token, location },
-      });
-      addUserToLocalStorage({ user, token, location });
-    } catch (error) {
-      dispatch({
-        type: REGISTER_USER_ERROR,
-        payload: { msg: error.response.data.msg },
-      });
-    }
+  const logoutUser = () => {
+    dispatch({ type: LOGOUT_USER });
+    removeUserFromLocalStorage();
   };
 
-  const loginUser = async (currentUser) => {
-    dispatch({ type: LOGIN_USER_BEGIN });
+  const setupUser = async ({ currentUser, endpoint, alertText }) => {
+    dispatch({ type: SETUP_USER_BEGIN });
     try {
-      const { data } = await axios.post('/api/v1/auth/login', currentUser);
-      const { user, token, location } = data;
+      const response = await axios.post(
+        `/api/v1/auth/${endpoint}`,
+        currentUser
+      );
+      const { user, token, location } = response.data;
       dispatch({
-        type: LOGIN_USER_SUCCESS,
-        payload: { user, token, location },
+        type: SETUP_USER_SUCCESS,
+        payload: { user, token, location, alertText },
       });
       addUserToLocalStorage({ user, token, location });
     } catch (error) {
       dispatch({
-        type: LOGIN_USER_ERROR,
+        type: SETUP_USER_ERROR,
         payload: { msg: error.response.data.msg },
       });
     }
@@ -87,15 +80,16 @@ export const AppProvider = ({ children }) => {
   useEffect(() => {
     setTimeout(() => {
       dispatch({ type: CLEAR_ALERT });
-    }, 5000);
+    }, 3000);
   }, [state.showAlert]);
   return (
     <AppContext.Provider
       value={{
         ...state,
         displayAlert,
-        registerUser,
-        loginUser,
+        setupUser,
+        toggleSidebar,
+        logoutUser,
       }}
     >
       {children}
