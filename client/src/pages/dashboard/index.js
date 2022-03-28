cb019a901f038f1508157aa9bc49422e80db074d
Amit <amitshuu@gmail.com>
2022-03-28 16:09:53 +0300

    Login screen setup

---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+import AllJobs from './AllJobs';
+import AddJob from './AddJob';
+import Stats from './Stats';
+import SharedLayouts from './SharedLayouts';
+import Profile from './Profile';
+
+export { AddJob, SharedLayouts, Stats, Profile, AllJobs };
