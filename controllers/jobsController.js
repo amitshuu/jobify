62a4b760e7cc77cd45eceb74beb4dc351d75a7bc
Amit <amitshuu@gmail.com>
2022-03-31 16:27:50 +0300

    Create job funcionality

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,3 +1,6 @@
+import BadRequestError from '../errors/bad-request.js';
+import Job from '../models/Job.js';
+
 const getAllJobs = (req, res) => {
   res.send('Get all jobs');
 };
@@ -14,8 +17,15 @@ const showStats = (req, res) => {
   res.send('Show stats');
 };
 
-const createJob = (req, res) => {
-  res.send('Create job');
+const createJob = async (req, res) => {
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequestError('Please provide all values!');
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(201).json({ job });
 };
 
 export { getAllJobs, updateJob, deleteJob, showStats, createJob };
