ada4a0648c0bd012e77e5a9548e0c83048edb147
Amit <amitshuu@gmail.com>
2022-04-05 16:16:29 +0300

    Jobs frontend components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,19 @@ import Alert from './Alert';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import BigSidebar from './BigSidebar';
-export { Logo, FormRow, Alert, BigSidebar, Navbar, SmallSidebar };
+import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
+import Jobs from './Jobs';
+export {
+  Logo,
+  FormRow,
+  Alert,
+  BigSidebar,
+  Navbar,
+  SmallSidebar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  Jobs,
+};
