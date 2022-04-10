c23f1679399cea32be511b28f5e955f90e24ebf7
Amit <amitshuu@gmail.com>
2022-04-10 13:28:35 +0300

    Typo

--- utils/checkPremissions.js
+++
@@ -1,9 +0,0 @@
-import UnAutonticatedError from '../errors/unauthenticated.js';
-
-const checkPremissions = (requestUser, resourceUserId) => {
-  if (requestUser.userId === resourceUserId.toString()) return;
-
-  throw new UnAutonticatedError('Not authorized to access this route!');
-};
-
-export default checkPremissions;
