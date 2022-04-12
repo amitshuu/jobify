358d3e5be80b1f77678e9f1ed53d8452440f81b2
Amit <amitshuu@gmail.com>
2022-04-12 18:48:41 +0300

    typo

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,4 @@
 import BadRequestError from './bad-request.js';
 import NotFoundError from './not-found.js';
-import UnAutonticatedError from './unAuthenticated.js';
+import UnAutonticatedError from './unauthenticated.js';
 export { BadRequestError, NotFoundError, UnAutonticatedError };
