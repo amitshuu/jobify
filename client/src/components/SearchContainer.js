01cfc6bdf79540fe355147754677785df766b178
Amit <amitshuu@gmail.com>
2022-04-12 17:14:16 +0300

    Job search funcionallity

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,7 +1,71 @@
 import React from 'react';
-
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { useAppContext } from '../context/appContext';
+import { FormRow } from '../components';
+import FormRowSelect from './FormRowSelect';
 const SearchContainer = () => {
-  return <div>SearchContainer</div>;
+  const {
+    search,
+    searchStatus,
+    searchType,
+    sortOptions,
+    isLoading,
+    handleChange,
+    statusOptions,
+    jobTypeOptions,
+    sort,
+    clearFilters,
+  } = useAppContext();
+
+  const handleSearch = (e) => {
+    handleChange({ name: e.target.name, value: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>Search form</h4>
+        <div className='form-center'>
+          <FormRow
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+            type='text'
+          />
+          <FormRowSelect
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          <FormRowSelect
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            type='submit'
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            Clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
