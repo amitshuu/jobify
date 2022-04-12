c372e36b8be6fb666e179984172145af78cc3c33
Amit <amitshuu@gmail.com>
2022-04-12 17:12:56 +0300

    Pagination funcionality

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -23,6 +23,8 @@ import {
   EDIT_JOB_ERROR,
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
+  CLEAR_FILTERS,
+  CHANGE_PAGE,
 } from './actions';
 
 import { initialState } from './appContext';
@@ -116,7 +118,7 @@ const reducer = (state, action) => {
   }
 
   if (action.type === HANDLE_CHANGE) {
-    return { ...state, [action.payload.name]: action.payload.value };
+    return { ...state, page: 1, [action.payload.name]: action.payload.value };
   }
   if (action.type === CLEAR_VALUES) {
     const initialState = {
@@ -223,6 +225,20 @@ const reducer = (state, action) => {
       monthlyApplications: action.payload.monthlyApplications,
       isLoading: false,
     };
+  }
+
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      sort: 'latest',
+      searchType: 'all',
+      searchStatus: 'all',
+      search: '',
+    };
+  }
+
+  if (action.type === CHANGE_PAGE) {
+    return { ...state, page: action.payload.page };
   }
   throw new Error(`no such action: ${action.type}`);
 };
