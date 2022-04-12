54472cf30bf6a05afde0ce39ba0b7226d0145d1a
Amit <amitshuu@gmail.com>
2022-04-12 17:12:31 +0300

    Get jobs by query

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -6,8 +6,44 @@ import mongoose from 'mongoose';
 import moment from 'moment';
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
-  res.status(200).json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const { status, jobType, sort, search } = req.query;
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+  if (status !== 'all') {
+    queryObject.status = status;
+  }
+  if (jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+
+  let result = Job.find(queryObject);
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(200).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
