a88ca4e180a6112b1f7f0ddac64daec9373af834
Amit <amitshuu@gmail.com>
2022-04-12 17:14:45 +0300

    Added Api limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,16 @@ import { register, login, updateUser } from '../controllers/authController.js';
 import authenticateUser from '../middleware/auth.js';
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+import rateLimiter from 'express-rate-limit';
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default router;
