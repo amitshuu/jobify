f86bf447b87d7d15adc2ad465ea3a18e181d23a3
Amit <amitshuu@gmail.com>
2022-04-12 17:13:55 +0300

    Pagination setup

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -3,12 +3,24 @@ import Wrapper from '../assets/wrappers/JobsContainer';
 import { useAppContext } from '../context/appContext';
 import Jobs from './Jobs';
 import Loading from './Loading';
+import PageBtnContainer from './PageBtnContainer';
 const JobsContainer = () => {
-  const { jobs, totalJobs, getJobs, isLoading } = useAppContext();
+  const {
+    jobs,
+    totalJobs,
+    getJobs,
+    isLoading,
+    searchStatus,
+    searchType,
+    search,
+    numOfPages,
+    sort,
+    page,
+  } = useAppContext();
 
   useEffect(() => {
     getJobs();
-  }, []);
+  }, [page, searchStatus, searchType, search, sort]);
 
   if (isLoading) {
     return <Loading center />;
@@ -17,7 +29,7 @@ const JobsContainer = () => {
   if (jobs.length < 1) {
     return (
       <Wrapper>
-        <h5>No jobs to display...</h5>;
+        <h5>No jobs to display...</h5>
       </Wrapper>
     );
   }
@@ -31,6 +43,7 @@ const JobsContainer = () => {
           return <Jobs key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
